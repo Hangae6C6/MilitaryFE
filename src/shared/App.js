08de230dfc0e931accde72b6a3f5b394536778ef
yooDeve <janhold@naver.com>
2022-04-23 15:09:40 +0900

    첫번째 커밋(기본 뼈대)

---
+++ src/shared/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      The world best team;
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
