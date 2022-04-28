f9b605e412c7450f9ff41db34e3eee6b61df1436
yooDeve <janhold@naver.com>
2022-04-28 15:04:46 +0900

    스타일 수정중

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,31 +1,46 @@
 import "./App.css";
-import { BrowserRouter, Router } from "react-router-dom";
-import { Route } from "react-router-dom";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Main from "../pages/Main";
 import Detail from "../pages/Main";
 import Login from "../pages/LogIn";
 import SignUp from "../pages/SignUp";
 import MyUser from "../pages/MyUser";
-import Chat from "../pages/ChatBot";
+import Chat from "../pages/Chat";
 import Test from "../pages/Test";
-
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-      <Route exact path='/' component={Main}></Route>
-      <Route exact path='/postDetail' component={Detail}></Route>
-      <Route exact path='/user/login' component={Login}></Route>
-      <Route exact path='/user/signup' component={SignUp}></Route>
-      <Route exact path='/mypage' component={MyUser}></Route>
-      {/* 챗봇페이지 라우팅, 컴포넌트 추후 수정 필요 */}
-      <Route exact path='/user/userdata' componenet={Chat}></Route>
-      <Route exact path='/user/userdata' componenet={Test}></Route>
-      </BrowserRouter>
-    </div>
+    <Router>
+      <Routes>
+        <Background>
+          <DivBox>
+            <Route exact path="/" element={<Main />}></Route>
+            <Route exact path="/postDetail" element={<Detail />}></Route>
+            <Route exact path="/user/login" element={<Login />}></Route>
+            <Route exact path="/user/signup" element={<SignUp />}></Route>
+            <Route exact path="/mypage" element={<MyUser />}></Route>
+            {/* 챗봇페이지 라우팅, 컴포넌트 추후 수정 필요 */}
+            <Route exact path="/user/userdata" element={<Chat />}></Route>
+            <Route exact path="/user/userdata" element={<Test />}></Route>
+          </DivBox>
+        </Background>
+      </Routes>
+    </Router>
   );
 }
+
+const DivBox = styled.div`
+  max-width: 375px;
+  max-height: 812px;
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+`;
+
+const Background = styled.body`
+  background-color: black;
+`;
 
 export default App;
