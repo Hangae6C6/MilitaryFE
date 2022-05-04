2977d93437c48874a602f8dd8d07cffc2e24e5f5
이성영 <syounglee012@gmail.com>
2022-05-04 04:15:10 -0700

    메인페이지 추가/수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,21 +1,40 @@
-//SY
-
 import React from "react";
 import styled from "styled-components";
-import Challenge from "../component/main/Challenge";
+import Card from "../component/main/Card";
+import { useDispatch, useSelector } from "react-redux";
+
+import { ActionCreators as postActions } from "../redux/modules/main";
+import { Box, Anchor, Text } from "grommet";
 
 const Main = () => {
-    return (
-        <MainBox>
-        <Challenge />
-        </MainBox>
-    );
-}
+  const dispatch = useDispatch();
+  const cards = useSelector((state) => state.card.cards);
+  console.log(cards);
+
+  React.useEffect(() => {
+    dispatch(postActions.getPostDB());
+  }, []);
+
+  return (
+    <MainBox>
+      <Box height="100px" width="200px" margin={{ left: "20px" }}>
+        <Text size="large" margin={{ top: "50px" }}>
+          안녕하십니까?
+        </Text>
+        <Anchor color="accent-4" href="#">
+          테스트 하러가기
+        </Anchor>
+      </Box>
+
+      <Card cards={cards} />
+    </MainBox>
+  );
+};
 
 export default Main;
 
 const MainBox = styled.div`
- background-color: #ffffff;
- width: 100%;
- height: 100%;
+  background-color: #ffffff;
+  width: 100%;
+  height: 100%;
 `;
---
+++ src/redux/modules/main.js
@@ -0,0 +1,50 @@
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+import axios from "axios";
+import { getCookie, setCookie, deleteCookie } from "../../shared/cookie";
+
+const GET_POST = "main/GET";
+
+const getPost = createAction(GET_POST, (cards) => ({
+  cards,
+}));
+
+const initialState = {
+  cards: [],
+};
+
+const getPostDB = () => {
+  return async function (dispatch, getState) {
+    try {
+      await axios({
+        method: "get",
+        url: "http://13.125.228.240/api/main",
+        headers: {
+          Authorization: `Bearer ${getCookie("token")}`,
+        },
+      }).then((response) => {
+        console.log(response);
+        dispatch(getPost(response));
+      });
+    } catch (err) {
+      console.log(err);
+      window.alert("GET 요청 실패");
+    }
+  };
+};
+
+export default handleActions(
+  {
+    [GET_POST]: (state, action) =>
+      produce(state, (draft) => {
+        draft.card.unshift(action.payload.cards.data);
+      }),
+  },
+  initialState
+);
+
+const ActionCreators = {
+  getPostDB,
+};
+
+export { ActionCreators };
