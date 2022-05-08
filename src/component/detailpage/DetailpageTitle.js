c643033273f2b85458544ace35a5966721c746e9
Young Lee <syounglee012@gmail.com>
2022-05-08 03:20:37 -0700

    폰트 임포트 수정

--- src/component/detailpage/DetailpageTitle.js
+++ src/component/detailpage/DetailpageTitle.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import Detail from "./Detail.css"
+
+
 
 const DetailpageTitle = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <ChallengeName>
+      <div id="title">느리게 살기 챌린지</div>
+    </ChallengeName>
+  );
 };
 
 export default DetailpageTitle;
+
+const ChallengeName = styled.div`
+
+  #title {
+    width: 375px;
+    height: 80px;
+    font-size: 24px;
+    color: #3f3f3f;
+    font-family: Gmarket Sans;
+    background-color: #6dbb91;
+    border-bottom: 2px solid #3f3f3f;
+  }
+`;
