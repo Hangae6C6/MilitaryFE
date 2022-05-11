835f18dff64afc6e5c7a41e21cd1f20bca4c1abe
yooDeve <janhold@naver.com>
2022-05-11 11:19:03 +0900

    채팅 css 90%완료

--- src/shared/App.js
+++ src/shared/App.js
@@ -73,11 +73,16 @@ const DivBox = styled.div`
   width: 100%;
   height: 100%;
   /* border: 1px solid black; */
+  border-radius: 3px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  /* overflow-y: auto; */
 `;
 
 const Background = styled.div`
   max-width: 100%;
   max-height: 100%;
+  width: 100vw;
+  height: 100vh;
   background-color: #ffffff;
 `;
 
